8e1b6ab6587320533b7f6023c9176a6417ca1be1
Toshio Ito <debug.ito@gmail.com>
2011-12-25 12:11:03 +0900

    Chnage handler interface of LoginPage.pm

--- engines/DebugIto/LoginPage.pm
+++ engines/DebugIto/LoginPage.pm
@@ -2,6 +2,9 @@ package Numpaar::Engine::DebugIto::LoginPage;
 use base 'Numpaar::Engine::DebugIto::Firefox';
 use Numpaar::Config qw(configElement);
 
+use strict;
+use warnings;
+
 sub new {
     my ($class, $user_name, $secret_filepath) = @_;
     my $self = $class->setupBasic('^Navigator\.Firefox \[LOGIN\]');
@@ -11,23 +14,48 @@ sub new {
 }
 
 sub handlerExtended_up {
-    my ($self, $connection, $want_help) = @_;
+    my ($self, $want_help) = @_;
+    my $connection = $self->getConnection();
     return 'LOGIN' if defined($want_help);
+    $self->handlerExtended_center();
+    $self->handlerExtended_right();
+    $self->handlerExtended_down();
+    return 0;
+}
+
+sub handlerExtended_center {
+    my ($self, $want_help) = @_;
+    my $connection = $self->getConnection();
+    return 'User name' if defined($want_help);
+    $connection->comKeyType($self->{account_name});
+    return 0;
+}
+
+sub handlerExtended_right {
+    my ($self, $want_help) = @_;
+    my $connection = $self->getConnection();
+    return 'Tab' if defined($want_help);
+    $connection->comKeyString("Tab");
+}
+
+sub handlerExtended_down {
+    my ($self, $want_help) = @_;
+    my $connection = $self->getConnection();
+    return 'Password' if defined($want_help);
     my $secret_loader = sprintf('%s "%s"', &configElement('extern_program', 'secret_loader'), $self->{secret_file});
     my $secret = `$secret_loader`;
     chomp $secret;
     if($secret eq '') {
         print STDERR "ERROR: LoginPage: secret loader error.\n";
-        $self->setState(0, $connection);
+        $self->setState(0);
         return 0;
     }
-    $connection->comKeyType($self->{account_name});
-    $connection->comKeyString("Tab");
     $connection->comKeyType($secret);
     $connection->comKeyString("Return");
-    $self->setState(0, $connection);
+    $self->setState(0);
     return 0;
 }
+
 
 1;
 
